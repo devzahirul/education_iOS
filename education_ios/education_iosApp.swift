04d65433a79d622c742681ec0533a417af3b768a
zahirulAIIUB <jislam150@gmail.com>
2025-07-10 11:32:17 +0600

    Initial Commit

---
+++ education_ios/education_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  education_iosApp.swift
+//  education_ios
+//
+//  Created by lynkto_1 on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct education_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
